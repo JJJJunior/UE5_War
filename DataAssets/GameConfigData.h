201d54f195c6d9a9792d2e89077ec32fe213bd6f
Kelvinsim <xj.master@gmail.com>
2025-06-15 22:29:40 +0800

    添加交互模块

---
+++ DataAssets/GameConfigData.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameConfigData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAR_API UGameConfigData : public UDataAsset
+{
+	GENERATED_BODY()
+};
